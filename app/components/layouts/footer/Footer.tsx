0ee072631dac6d72567de5f85bdf727ca897e820
273* <keita.y310@icloud.com>
2024-03-10 15:39:28 +0900

    表示するコンテンツを増やしました．

--- app/components/layouts/footer/Footer.tsx
+++ app/components/layouts/footer/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
   const { setTheme } = useTheme();
 
   return (
-    <div className="sticky bottom-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="fixed  bottom-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <p>Footer</p>
         <div>
