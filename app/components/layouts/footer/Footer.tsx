58596ea38b48f514063ca08764a2cd407bb763c5
273* <keita.y310@icloud.com>
2024-03-13 22:14:48 +0900

    フッターを実装しました．

--- app/components/layouts/footer/Footer.tsx
+++ app/components/layouts/footer/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-// import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
@@ -11,36 +10,48 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  SiX,
+  SiGithub,
+  SiYoutube,
+  SiInstagram,
+} from "@icons-pack/react-simple-icons";
 
 const Footer = () => {
   const { setTheme } = useTheme();
 
   return (
-    <div className="fixed  bottom-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <p>Footer</p>
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              {/* <DropdownMenuItem onClick={() => setTheme("system")}>
+    <div className="fixed bottom-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
+        <p>273DoWorks</p>
+        <nav className="fixed right-0 mr-10 flex items-center justify-end gap-4">
+          <SiGithub className="size-[1.2rem] duration-150 hover:opacity-50" />
+          <SiX className="size-[1.2rem] duration-150 hover:opacity-50" />
+          <SiYoutube className="size-[1.2rem] duration-150 hover:opacity-50" />
+          <SiInstagram className="size-[1.2rem] duration-150 hover:opacity-50" />
+          <div className="ml-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                {/* <DropdownMenuItem onClick={() => setTheme("system")}>
                 System
               </DropdownMenuItem> */}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </nav>
       </div>
     </div>
   );
--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className=" h-14 max-w-screen-2xl items-center justify-between">
         <p>Header</p>
       </div>
     </div>
