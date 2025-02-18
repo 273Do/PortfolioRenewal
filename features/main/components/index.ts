48b76bdbbc955ad87a9309b2bbd308130d7cdf62
273* <keita.y310@icloud.com>
2025-02-18 14:39:13 +0900

    ✨ ロゴの3D表示に対応しました．

--- features/main/components/index.ts
+++ features/main/components/index.ts
@@ -1,7 +1,8 @@
 import GitCalendar from "./GitCalendar/GitCalendar";
 import Hobby from "./Hobby/Hobby";
+import LogoThree from "./LogoThree/LogoThree";
 import Notice from "./Notice/Notice";
 import Profile from "./Profile/Profile";
 import Welcome from "./Welcome/Welcome";
 
-export { GitCalendar, Hobby, Notice, Profile, Welcome };
+export { GitCalendar, Hobby, Notice, Profile, Welcome, LogoThree };
