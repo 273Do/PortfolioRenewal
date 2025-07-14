a83bd9c9b70e06526882f374a4b24ff91d8b71a0
273* <keita.y310@icloud.com>
2025-07-14 22:10:00 +0900

    ✨ アクティビティエリアの画像のレスポンシブに対応

--- features/main/components/ActivitySection/ImagesArea.tsx
+++ features/main/components/ActivitySection/ImagesArea.tsx
@@ -12,7 +12,7 @@ const RandomImageList = async () => {
       {galleryData.map((data: GalleryObj) => (
         <div
           // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-          className="group relative grayscale duration-200 before:absolute before:inset-0 before:rounded-md before:bg-black before:bg-opacity-20 before:content-[''] hover:grayscale-0 sm:mb-4"
+          className="group relative mb-2 grayscale duration-200 before:absolute before:inset-0 before:rounded-md before:bg-black before:bg-opacity-20 before:content-[''] hover:grayscale-0 sm:mb-4"
           key={data.sys.id}
         >
           <Image
@@ -24,10 +24,12 @@ const RandomImageList = async () => {
           />
           <div className="absolute inset-0 flex flex-col justify-between p-3 text-white opacity-0 duration-200 group-hover:opacity-100">
             <div className="relative">
-              <h1 className="text-3xl font-bold">{data.title}</h1>
-              <p className="font-sm font-light">{data.description}</p>
+              <h1 className="text-xl font-bold sm:text-3xl">{data.title}</h1>
+              <p className="text-xs font-light sm:text-sm">
+                {data.description}
+              </p>
             </div>
-            <p className="text-xs font-light">
+            <p className="text-xs font-light sm:text-sm">
               {format(
                 new Date(data.eventDate).toLocaleDateString(),
                 "yyyy-MM-dd"
