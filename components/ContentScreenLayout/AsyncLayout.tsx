71f27866d0bd8b44fccfd0866d179b3e093be7fa
273* <keita.y310@icloud.com>
2025-07-17 22:13:30 +0900

    🩹 スマホ表示においてレイアウトが崩れる問題を修正

--- components/ContentScreenLayout/AsyncLayout.tsx
+++ components/ContentScreenLayout/AsyncLayout.tsx
@@ -6,7 +6,7 @@ const AsyncLayout = async ({
   className?: string;
 }) => {
   return (
-    <div className={`flex h-[calc(100vh-5rem)] items-end ${className}`}>
+    <div className={`flex h-[calc(100svh-5rem)] items-end ${className}`}>
       <div className="flex size-full flex-col items-end justify-between pt-0">
         {children}
       </div>
